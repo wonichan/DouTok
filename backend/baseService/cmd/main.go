df580ade5429641dae63d799db97b3f947ce7f41
wonichan <wonichan949@163.com>
2025-03-25 22:52:46 +0800

    222

--- backend/baseService/cmd/main.go
+++ backend/baseService/cmd/main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import (
+	"DouTok-example/backend/baseService/internal/conf"
+	"DouTok-example/backend/common/launcher"
+)
 
+func main() {
+	c := &conf.Config{}
+	launcher.New()
 }
